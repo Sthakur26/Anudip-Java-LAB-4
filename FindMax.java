f318e93361557bdbebe4eaf6433ed3a2e6572186
Supriya_Thakur <72198633+Sthakur26@users.noreply.github.com>
2023-10-26 22:11:16 +0530

    Create FindMax.java

---
+++ FindMax.java
@@ -0,0 +1,8 @@
+public class FindMax{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 30;
+        int max = (num1 > num2) ? num1 : num2;
+        System.out.println("Maximum between " + num1 + " and " + num2 + " is: " + max);
+    }
+}
